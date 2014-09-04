320183628e222f499b00dc2a4c8f7f48f021517e
hb08 <fated.decree@gmail.com>
2014-09-04 19:29:41 -0400

    Fully Functioning

--- Betancourt_Harmony_Madlib/main.py
+++ Betancourt_Harmony_Madlib/main.py
@@ -5,7 +5,7 @@ __author__ = 'Harmony Betancourt'
     Purpose: Create a mad lib that collects user information and populates the output
 '''
 # Variables for set text in dictionary
-messages = {"greeting": "Welcome to the MadLibs Game!", "goodbye": "Thanks for playing!", "start": "It was only ", "two": " days before Halloween when the dead rose from their graves. ", "three": " was prepared. With a ", "four":  " as a weapon.", "five": " With the help of ", "six": "who weilded a hardcover copy of ", "seven": " managed to kill the ", "eight":  "zombies between Full Sail University and the military zone. There were", "nine": " other survivors, all members of the ", "ten": " team. It was going to be a very hard year for ", "eleven": " year old ", "end": ", a very hard year indeed."}
+messages = {"greeting": "Welcome to the MadLibs Game!", "goodbye": "Thanks for playing!", "start": "It was only ", "two": " days before Halloween when the dead rose from their graves. ", "three": " was prepared. With a ", "four":  " as a weapon, ", "five": ", with the help of ", "six": ", who weilded a hardcover copy of ", "seven": ", managed to kill the ", "eight":  " zombies between Full Sail University and the military zone. There were ", "nine": " other survivors, all members of the ", "ten": " team. It was going to be a very hard year for ", "eleven": " year old ", "end": ", a very hard year indeed."}
 
 # FUNCTION: Collect User Input in Lists
 def getNumbers():
@@ -29,9 +29,11 @@ def newNum(age, pick):
     import random
     ran = random.randint(1, 10)
     if age>pick:
-        return age/ran
+        new = age/ran
+        numbers.append(new)
     else:
-        return pick/ran
+        new = pick/ran
+        numbers.append(new)
 
 # Populate User Input as numbers array and words dictionary
 numbers = getNumbers()
@@ -39,14 +41,14 @@ newNum(numbers[1], numbers[2])
 words = getWords()
 
 # For loop to turn all numbers into strings and extend to words array
-for number in numbers:
-    a = str(number)
-    words.extend(a)
+for n in numbers:
+    a = str(n)
+    words.append(a)
 
-# Favorite Number 6, Name 0, favorite color 5, item 1, Name 0, actor 3, book 2, added 8, random number 8, sport 4, age 7, name 0
-madLib = messages["start"] + words[6] + messages["two"] + words[0] + messages["three"] + words[5] + " " + words[1] + messages["four"] + words[0] + messages["five"] + words[3] + messages["six"] + words[3] + messages["seven"] + words[2] + messages["eight"] + words[8] + messages["nine"] + words[9] + messages["ten"] + words[4] + messages["eleven"] + words[7] + words[0] + messages["end"]
+# Favorite Number 6, Name 0, favorite color 5, item 1, Name 0, actor 3, book 2, added 9, random number 8, sport 4, age 7, name 0
+mad_lib = messages["start"] + words[6] + messages["two"] + words[0] + messages["three"] + words[5] + " " + words[1] + messages["four"] + words[0] + messages["five"] + words[3] + messages["six"] + words[2] + messages["seven"] + words[6] + messages["eight"] + words[8] + messages["nine"] + words[4] + messages["ten"] + words[7] + messages["eleven"] + words[0] + messages["end"]
 
-print madLib
+print mad_lib
 
 
 

03bb5f7524063b7fe3efa253b90e3123d83a0b0b
hb08 <fated.decree@gmail.com>
2014-09-04 00:01:53 -0400

    Start from 4th number

--- Betancourt_Harmony_Madlib/main.py
+++ Betancourt_Harmony_Madlib/main.py
@@ -33,17 +33,7 @@ if numbers[1]>numbers[2]:
 else:
     numbers.append(numbers[0] + numbers[1])
 
-# Function to put it all together
-def createML(numbers, words):
-    message = '''
-        "Hello, my name is {words.name}, and I never leave the house without my {words.item}. Let me tell you exactly why. I was walking to bookclub. I'd already skipped it {numbers[0]} times, but that night we were talking about {words.book}! I was in a hurry, and ran into {words.actor}! I was just about to ask for a picture, when a riot broke down after the local {words.sport} team lost! {words.actor} and I were torn apart by the crowd. That's when I saw it! A {words.color} {words.item}. I quickly grabbed the {words.color} {words.item}, spun it over my head {numbers[2]} times, and threw it. Like magic, it landed at the feet of {words.actor}. Never in my {numbers[1]} years of life did I expect that {words.actor} would sign it, then crowdsurf the rioting {words.sport} fans to deliver it to me. We fell in love amidst the chaos, and that, children, is how I met {words.actor}, and we've been happily married for the past {numbers[3]} years."
-    '''
-    message = message.format(**locals())
-    return message
 
-madlib = createML(numbers, words)
-
-print madlib
 
 
 

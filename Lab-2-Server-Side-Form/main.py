3308ed53d1a67a0b924a7c3116317be0e1a2b681
hb08 <fated.decree@gmail.com>
2014-09-09 07:55:38 -0400

    Divided right and code is working so far

--- Lab-2-Server-Side-Form/main.py
+++ Lab-2-Server-Side-Form/main.py
@@ -8,12 +8,37 @@ Simple Form
 import webapp2
 from sites import Site
 
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         site = Site()
 
+        # If the page has a Get request
+        if self.request.GET:
+            # Set all the variables to user inputs
+            contact = self.request.GET['contact']
+            phone = self.request.GET['phone']
+            response = self.request.GET['response']
+            email = self.request.GET['email']
+            message = self.request.GET['message']
+
+            # Messages to Print
+            thanks = "<br/>We read every message we receive, and will give yours the consideration it deserves."
+            # Set Body with beginning of message
+            b = "Thank you, " + contact + ", for your " + " message of <span>'" + message + "'</span> " + thanks
+            site.body = b + "<br/>We will be in contact by " + response
+            # Add in personalized response
+            if response == "phone":
+                site.body += " at number " + phone
+            elif response == "email":
+                site.body += " at email address " + email
+        # Otherwise
+        else:
+            # Show the form
+            site.body = site.contact_form
+
         # Print Result Page
-        self.response.write(site.print_page)
+        self.response.write(site.header + site.body + site.closer)
 
 
 app = webapp2.WSGIApplication([

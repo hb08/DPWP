70330e341b2f1e3d63fbb7cfa9a1938b846b1d2b
hb08 <fated.decree@gmail.com>
2014-09-24 19:10:07 -0400

    Formatting is a bit more clear
    More code broken down by MVC category

--- betancourt-harmony-api/model.py
+++ betancourt-harmony-api/model.py
@@ -1,13 +1,13 @@
 """ This random generator model handles fetching, parsing, and sorting data from the api. """
 import urllib2  # Python classes and code needed to request/recieve/open url info
 import json
-import time  # Will translate epoch format for dob into something readable
+
 
 
 class RgModel(object):
     def __init__(self):
         self._userinput = ''  # Form set by main
-        self._results = ''
+        self._results = {}
 
     @property
     def userinput(self):  # Getter for user input
@@ -41,13 +41,13 @@ class RgModel(object):
         # Parse with JSON
         api_json = json.load(fromapi)
         # Get information from JSON
-        u = api_json['results'][0]['user']['username']   # Get alias username
+        us = api_json['results'][0]['user']['username']   # Get alias username
         ng = api_json['results'][0]['user']['gender']   # Get alias gender
         e = api_json['results'][0]['user']['email']   # Get alias email
         dob = float(api_json['results'][0]['user']['dob'])  # Get alias birthday in epoch as a float
         p = api_json['results'][0]['user']['phone']   # Get alias phone
         c = api_json['results'][0]['user']['cell']   # Get alias cell phone
-        title = api_json['results'][0]['user']['name']['title']  # Get alias title
+        t = api_json['results'][0]['user']['name']['title']  # Get alias title
         first = api_json['results'][0]['user']['name']['first']  # Get alias first name
         last = api_json['results'][0]['user']['name']['last']  # Get alias last name
         street = api_json['results'][0]['user']['location']['street']  # Get alias street
@@ -55,21 +55,11 @@ class RgModel(object):
         state = api_json['results'][0]['user']['location']['state']  # Get alias  state
         zip = api_json['results'][0]['user']['location']['zip']  # Get alias zip
         image = api_json['results'][0]['user']['picture']['medium']  # Get alias image - medium size
-
-        # Formatting and simplifying
-        username = "Username: " + u + "<br/>"  # Add label and break
-        new_gender = "Gender: " + ng.capitalize() + "<br/>"  # Add label, capitalize, and break
-        email = "Email: " + e + "<br/>"  # Add label and break
-        dob = time.strftime('%B %d %Y', time.localtime(dob))  # DOB now is in local readable time
-        bday = "Birthday: " + dob + "<br/>"  # Add label and break
-        phone = "Phone Number: " + p + "<br/>"  # Add label and break
-        cell = "Mobile Number: " + c + "<br/>"  # Add label and break
-        image_ready = '<img src="' + image + '" alt="Your Pretty Picture" /><br/>'  # format image
-
         # Put all elements of name into one name with capitalized first letters
-        name = "Name: " + title.capitalize() + " " + first.capitalize() + " " + last.capitalize() + "<br/>"
+        name = "Name: " + t.capitalize() + " " + first.capitalize() + " " + last.capitalize() + "<br/>"
         # Put all address elements into one variable with title format on the words
-        address = street.title() + " " + city.title() + ", " + state.title() + " " + zip
+        add = street.title() + " " + city.title() + ", " + state.title() + " " + zip
 
-        # Return results
-        self._results = image_ready + name + new_gender + username + bday + email + phone + cell + address
+        # Put it in the container
+        self._results = {'us': us, 'g': ng, 'e': e, 'dob': dob, 'p': p, 'c': c, 't': t, 'n': name, 'a': add, 'i': image}
+
--- betancourt-harmony-api/view.py
+++ betancourt-harmony-api/view.py
@@ -1,5 +1,6 @@
 """ This class handles how the data from the model is shown to the user """
 from model import RgModel
+import time  # Will translate epoch format for dob into something readable
 
 
 class Layout(object):  # Class to create layout
@@ -50,6 +51,8 @@ class IndexPage(Layout):  # Makes a layout object called IndexPage
         self._form_close = '</form>'  # Form closing tag
         self.__form_content = []  # Private and Protected
         self._form = '<form method="GET">'  # Form opening tag, rest filled in with getter/setter
+        self._format_results = ''
+        print self._format_results
 
     @property
     def form(self):  # Getter for form
@@ -73,7 +76,25 @@ class IndexPage(Layout):  # Makes a layout object called IndexPage
         self._form += self._form_close  # Final form is everything so far with the closing form tag
         self.content = self.form  # Adds full form to content
 
+    @property
+    def format_results(self):  # Getter for results
+        # Formatting and simplifying
+        formatted = "<div class='rg'>"  # Begin formatting
+        f_end = "</div>"
+        username = "Username: " + self.v.results['us'] + "<br/>"  # Add label and break
+        new_gender = "Gender: " + self.v.results['g'].capitalize() + "<br/>"  # Add label, capitalize, and break
+        email = "Email: " + self.v.results['e'] + "<br/>"  # Add label and break
+        dob = time.strftime('%B %d %Y', time.localtime(self.v.results['dob']))  # DOB now is local readable time
+        bday = "Birthday: " + dob + "<br/>"  # Add label and break
+        phone = "Phone Number: " + self.v.results['p'] + "<br/>"  # Add label and break
+        cell = "Mobile Number: " + self.v.results['c'] + "<br/>"  # Add label and break
+        image_ready = '<img src="' + self.v.results['i'] + '" alt="Your Pretty Picture" /><br/>'  # format image
+
+        # Return results
+        formatted += image_ready + new_gender + username + bday + email + phone + cell + self.v.results['a'] + f_end
+        return formatted
+
     # Override default page
     def page(self):
         # Add in form and results
-        return self.header + self.content + self.v.results + self.footer
+        return self.header + self.content + self.format_results + self.footer
